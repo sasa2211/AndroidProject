a728733b2447aa6dfb9af7e09d3e779396a6f04b
zhangjin <zhangjin@stepelectric.com>
2023-05-08 17:19:30 +0800

    for循环提交

--- app/src/main/java/com/step/aidl/myapplication/MainActivity.java
+++ app/src/main/java/com/step/aidl/myapplication/MainActivity.java
@@ -3,11 +3,24 @@ package com.step.aidl.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void initView(){
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 10; i ++){
+            items.add("i === " + String.valueOf(i));
+
+        }
+        for (String item : items){
+            System.out.println(item);
+        }
     }
 }
